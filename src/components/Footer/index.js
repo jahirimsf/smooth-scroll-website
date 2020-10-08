d78b7ec1e286061a11fa0ffa44eeb5e1377f007e
Jahir <jahir.imsf@gmail.com>
2020-10-08 20:38:57 +0600

    Evrything is perfectly working except react route and in the signin part functionality isn't added yet

---
+++ src/components/Footer/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { animateScroll as scroll } from "react-scroll";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinkContainer,
+  FooterLinkWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+} from "./FooterElements";
+
+const Footer = () => {
+  const homeToggle = () => {
+    scroll.scrollToTop();
+  };
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinkContainer>
+          <FooterLinkWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About us</FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/">Contact</FooterLink>
+              <FooterLink to="/">Support</FooterLink>
+              <FooterLink to="/">Destination</FooterLink>
+              <FooterLink to="/">Sponsorship</FooterLink>
+            </FooterLinkItems>
+          </FooterLinkWrapper>
+          <FooterLinkWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/">Submit Video</FooterLink>
+              <FooterLink to="/">Ambassadors</FooterLink>
+              <FooterLink to="/">Agency</FooterLink>
+              <FooterLink to="/">Influencer</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/">Instagram</FooterLink>
+              <FooterLink to="/">Facebook</FooterLink>
+              <FooterLink to="/">Youtube</FooterLink>
+              <FooterLink to="/">Twitter</FooterLink>
+            </FooterLinkItems>
+          </FooterLinkWrapper>
+        </FooterLinkContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/" onClick={homeToggle}>
+              dolla
+            </SocialLogo>
+            <WebsiteRights>
+              dolla © {new Date().getFullYear()}
+              All rights reserved
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Services/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Icon1 from "../../images/svg-7.svg";
+import Icon2 from "../../images/svg-10.svg";
+import Icon3 from "../../images/svg-4.svg";
+
+import {
+  ServicesContainer,
+  ServicesH1,
+  ServicesWrapper,
+  ServicesIcon,
+  ServicesCard,
+  ServicesH2,
+  ServicesP,
+} from "./ServicesElements";
+const Services = () => {
+  return (
+    <>
+      <ServicesContainer id="services">
+        <ServicesH1>Our Services</ServicesH1>
+        <ServicesWrapper>
+          <ServicesCard>
+            <ServicesIcon src={Icon1} />
+            <ServicesH2>Reduce expanses</ServicesH2>
+            <ServicesP>
+              We help reduce your fess increase your overall revenue
+            </ServicesP>
+          </ServicesCard>
+          <ServicesCard>
+            <ServicesIcon src={Icon2} />
+            <ServicesH2>Virtual Offices</ServicesH2>
+            <ServicesP>
+              You can access our platform online anywhere in the world.
+            </ServicesP>
+          </ServicesCard>
+          <ServicesCard>
+            <ServicesIcon src={Icon3} />
+            <ServicesH2>Premium Benefits</ServicesH2>
+            <ServicesP>
+              Unlock our special memership card that returns 5% cash cack.
+            </ServicesP>
+          </ServicesCard>
+        </ServicesWrapper>
+      </ServicesContainer>
+    </>
+  );
+};
+
+export default Services;
